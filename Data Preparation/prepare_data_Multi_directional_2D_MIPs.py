ce927fe071ef7eb9f0707de2c31e6817f3a7d6e7
sambitarai17@gmi17@§§§ls <sambitarai17@gmail.com>
2023-05-08 15:59:14 +0200

    prepare_data (Multi_directional_2D_MIPs based on different  CT windows added)

--- Data Preparation/prepare_data_Multi_directional_2D_MIPs.py
+++ Data Preparation/prepare_data_Multi_directional_2D_MIPs.py
@@ -1,1 +1,79 @@
 import numpy as np
+import pandas as pd
+import SimpleITK as sitk
+from tqdm import tqdm
+import scipy.ndimage
+import cv2
+import os
+
+import sys
+sys.path.insert(0, '/media/sambit/HDD/Sambit/Projects/Project_5/Framework/Proposed/2D_UNET')
+from utils import read_nii, generate_all_MIPs_SUV, generate_all_MIPs_CT, preprocess_df, preprocess_CT_HU_values
+from config import parse_args
+
+def main(args):
+	path_data = args.data_path
+	path_output = args.path_data_MIPs
+	df = pd.read_csv(args.path_df) #DataFrame containing all the original tumorous scans.
+	#df = df[df["diagnosis"]=="LYMPHOMA"].reset_index(drop=True)
+	df = preprocess_df(df)
+	#print("124 to end")
+	#df = df[64:].reset_index(drop=True)
+
+	for idx, row in tqdm(df.iterrows(), total=len(df)):
+		pat_ID = row["pat_ID"]
+		scan_date = row["scan_date"]
+
+		SUV = read_nii(row["SUV"])
+		SUV_B = read_nii(row["SUV_bone"])
+		SUV_LT = read_nii(row["SUV_lean"])
+		SUV_AT = read_nii(row["SUV_adipose"])
+		SUV_A = read_nii(row["SUV_air"])
+
+		CT = read_nii(row["CT"])
+		CT_B = read_nii(row["CT_bone"])
+		CT_LT = read_nii(row["CT_lean"])
+		CT_AT = read_nii(row["CT_adipose"])
+		CT_A = read_nii(row["CT_air"])
+
+		CT_LT, CT_AT, CT_A = preprocess_CT_HU_values(CT_LT), preprocess_CT_HU_values(CT_AT), preprocess_CT_HU_values(CT_A)
+
+		SEG = read_nii(row["SEG"])
+
+		#Generate different multi-directional SUV channels based on CT HU values
+		save_path = os.path.join(path_output, pat_ID, scan_date)
+		if not os.path.exists(save_path):
+			os.makedirs(save_path)
+
+		if not os.path.exists(os.path.join(save_path, "SUV_MIP")): #SUV_MIP, SUV_bone, SUV_lean, SUV_adipose, SUV_air
+			os.makedirs(os.path.join(save_path, "SUV_MIP"))
+		if not os.path.exists(os.path.join(save_path, "SUV_bone")):
+			os.makedirs(os.path.join(save_path, "SUV_bone"))
+		if not os.path.exists(os.path.join(save_path, "SUV_lean")):
+			os.makedirs(os.path.join(save_path, "SUV_lean"))
+		if not os.path.exists(os.path.join(save_path, "SUV_adipose")):
+			os.makedirs(os.path.join(save_path, "SUV_adipose"))
+		if not os.path.exists(os.path.join(save_path, "SUV_air")):
+			os.makedirs(os.path.join(save_path, "SUV_air"))
+
+		if not os.path.exists(os.path.join(save_path, "CT_MIP")): #SUV_MIP, SUV_bone, SUV_lean, SUV_adipose, SUV_air
+			os.makedirs(os.path.join(save_path, "CT_MIP"))
+		if not os.path.exists(os.path.join(save_path, "CT_bone")):
+			os.makedirs(os.path.join(save_path, "CT_bone"))
+		if not os.path.exists(os.path.join(save_path, "CT_lean")):
+			os.makedirs(os.path.join(save_path, "CT_lean"))
+		if not os.path.exists(os.path.join(save_path, "CT_adipose")):
+			os.makedirs(os.path.join(save_path, "CT_adipose"))
+		if not os.path.exists(os.path.join(save_path, "CT_air")):
+			os.makedirs(os.path.join(save_path, "CT_air"))
+
+		if not os.path.exists(os.path.join(save_path, "SEG")):
+			os.makedirs(os.path.join(save_path, "SEG"))
+
+		generate_all_MIPs_SUV(save_path, SUV, SUV_B, SUV_LT, SUV_AT, SUV_A, SEG, args.SUV_min, args.SUV_max, args.rotation_min, args.rotation_max, args.rotation_interval)
+		generate_all_MIPs_CT(save_path, CT, CT_B, CT_LT, CT_AT, CT_A, SEG, args.CT_min, args.CT_max, args.rotation_min, args.rotation_max, args.rotation_interval)
+
+if __name__ == "__main__":
+	args = parse_args()
+	main(args)
+	print("Done")
--- config.py
+++ config.py
@@ -10,7 +10,7 @@ def parse_args():
     parser.add_argument("--path_df", default="/media/sambit/HDD/Sambit/Projects/U-CAN/autoPET_2022/Data/DataFrame_with_Paths/df_disease_wise.csv", help="Path containing all the Patient's Data (CT, SUV, SEG).")
     parser.add_argument("--MIP_directions", default=["coronal", "saggital"], help="Path containing all the Patient's Data.")
 
-    parser.add_argument("--output_path", default="/media/sambit/HDD/Sambit/Projects/Project_5/Framework/Data_Preparation/Output", help="Path containing all the Patient's Data.")
+    parser.add_argument("--output_path", default="/media/sambit/HDD/Sambit/GitHub/Tumor Segmentation followed by Outcome Prediction/Tumor-segmentation-from-PET-CT-followed-by-outcome-prediction/Data Preparation/Output", help="Path containing all the Patient's Data.")
     #parser.add_argument("--include_ids", default=["PETCT_0b98dbe00d", "PETCT_13b40a817b", "PETCT_30c4b7062b", "PETCT_39159c05c2", "PETCT_335a00191d", "PETCT_3b1c9155f5", "PETCT_5a58935b68", "PETCT_5e2da717db", "PETCT_7a77b26403", "PETCT_92c5c944a5", "PETCT_99a7bfad23", "PETCT_7948aa0e26", "PETCT_983a76fd43", "PETCT_9b982e72cb", "PETCT_bf178a41b2", "PETCT_b327726c24", "PETCT_ca58410fad", "PETCT_d4b2ff9721", "PETCT_e03b96666f", "PETCT_ef9d41b836", "PETCT_0cda25453b", "PETCT_1b1bdfc35b", "PETCT_11e258cc1f", "PETCT_1956667fce", "PETCT_2b60c8135a", "PETCT_30001118d0", "PETCT_55ca11402a", "PETCT_5d10be5b89", "PETCT_6a3477cd9a", "PETCT_8a52353a72", "PETCT_8ec13728df", "PETCT_685d7c09b5", "PETCT_901573a747", "PETCT_63508c679d", "PETCT_790246c76c", "PETCT_ac75e49284", "PETCT_ad7cd4a9d2", "PETCT_aa27cb9156", "PETCT_c29aba73da", "PETCT_b53ba7c6bf", "PETCT_ddbb3c69f0", "PETCT_ea42c88cc7", "PETCT_ea051a3e6c", "PETCT_fe705ea1cc", "PETCT_2f7200f771", "PETCT_4c75fa4a5d", "PETCT_4b688f46b0", "PETCT_a82f03863a", "PETCT_e252be4334"], help="Path containing all the Patient's Data.")
 
     parser.add_argument("--bone_HU", default=[200], help="Bone HU limit (x > +200)")
@@ -18,7 +18,7 @@ def parse_args():
     parser.add_argument("--adipose_HU", default=[-190, -30], help="Adipose tissue HU limit (-190 < x < -30)")
     parser.add_argument("--air_HU", default=[-191], help="Air HU limit (< -191)")
 
-    parser.add_argument("--SUV_max_collage", default=14, help="Maximum SUV to be used during generation of collages for visualization")
+    parser.add_argument("--SUV_max_collage", default=15, help="Maximum SUV to be used during generation of collages for visualization")
 
     args = parser.parse_args()
     return args
